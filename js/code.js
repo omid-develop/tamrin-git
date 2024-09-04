11b49924a0a13227c5e662cc5e236a6afc33054c
omid-develop <omidataee20041382@gmail.com>
2024-09-04 16:53:57 +0330

    okk edit

--- js/code.js
+++ js/code.js
@@ -1,2 +1,15 @@
 console.log("omid");
 console.log("hassan");
+
+
+const auth = ()=>{
+
+    const user = "omid";
+
+    if (user === 200){
+        document.write("okkkk");
+    }else{
+        document.write("Not okkk");
+    }
+
+}
